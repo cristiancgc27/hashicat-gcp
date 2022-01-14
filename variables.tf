c20475a051b6d8f2063d83caee92f6cf61af3154
cristiancgc27 <84590569+cristiancgc27@users.noreply.github.com>
2022-01-14 11:44:35 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -47,3 +47,8 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+variable "gcp_credentials" {
+  type = string
+  sensitive = true
+  description = "Google Cloud service account credentials"
+}
